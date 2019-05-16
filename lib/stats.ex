5bdd87566fd75bd0051f9a8786e3dfc1bf25149e
Jake Prem <jakeprem@gmail.com>
2019-05-16 09:59:27 -0400

    SMRT-1186 Add docs and make reducer private
    co-authored-by: Matthew Vance <matthew.vance@accenture.com>

--- lib/stats.ex
+++ lib/stats.ex
@@ -1,11 +1,19 @@
 defmodule ScosSystemTest.Stats do
+  @moduledoc """
+  Functions for aggregating the stats stored by Flair
+  """
+
+  @doc """
+  Aggregate a list of records (as maps) returned from Flair's operational_stats table
+  """
+  @spec aggregate(list(map())) :: map()
   def aggregate(stats_list) do
     stats_list
     |> Enum.map(&Map.get(&1, "stats"))
     |> Enum.reduce(%{"count" => 0, "average" => 0}, &reducer/2)
   end
 
-  def reducer(item, acc) do
+  defp reducer(item, acc) do
     %{
       "average" => combine_averages(item, acc),
       "min" => min(item["min"], acc["min"]),
