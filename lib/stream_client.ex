a1d692fb38b2d7101e86a937dcbaf6e00212b7dc
Timothy Regan <tregan@pillartechnology.com>
2020-03-25 09:33:16 -0400

    Now testing streams, general refactor
    smartcolumbusos/scos-system-test#21

---
+++ lib/stream_client.ex
@@ -0,0 +1,79 @@
+defmodule ScosSystemTest.StreamClient do
+  @moduledoc false
+  require Logger
+  alias Phoenix.Channels.GenSocketClient
+  @behaviour GenSocketClient
+
+  @rejoin_interval_seconds 10
+
+  def start_link(url) do
+    GenSocketClient.start_link(
+          __MODULE__,
+          Phoenix.Channels.GenSocketClient.Transport.WebSocketClient,
+          url
+        )
+  end
+
+  def init(url) do
+    {:connect, url, [], %{}}
+  end
+
+  def join_topic(pid, topic) do
+    Process.send(pid, {:join, topic}, [])
+  end
+
+  def handle_connected(_transport, state) do
+    Logger.info("connected")
+    {:ok, state}
+  end
+
+  def handle_disconnected(reason, state) do
+    Logger.error("disconnected: #{inspect reason}")
+    Process.send_after(self(), :connect, :timer.seconds(@rejoin_interval_seconds))
+    {:ok, state}
+  end
+
+  def handle_joined(topic, _payload, _transport, state) do
+    Logger.info("joined the topic #{topic}")
+    :ets.new(String.to_atom(topic), [:named_table])
+    {:ok, state}
+  end
+
+  def handle_join_error(topic, payload, _transport, state) do
+    Logger.warn("join error on the topic #{topic}: #{inspect payload}\nRetrying in #{@rejoin_interval_seconds} seconds")
+    Process.send_after(self(), {:join, topic}, :timer.seconds(@rejoin_interval_seconds))
+    {:ok, state}
+  end
+
+  def handle_channel_closed(topic, payload, _transport, state) do
+    Logger.error("disconnected from the topic #{topic}: #{inspect payload}")
+    Process.send_after(self(), {:join, topic}, :timer.seconds(@rejoin_interval_seconds))
+    {:ok, state}
+  end
+
+  def handle_message(topic, event, payload, _transport, state) do
+    Logger.debug("message on topic #{topic}: #{event} #{inspect payload}")
+    :ets.insert(String.to_existing_atom(topic), {NaiveDateTime.utc_now(), payload})
+    {:ok, state}
+  end
+
+  def handle_reply(topic, _ref, payload, _transport, state) do
+    Logger.debug("reply on topic #{topic}: #{inspect payload}")
+    {:ok, state}
+  end
+
+  def handle_info(:connect, _transport, state) do
+    Logger.info("connecting")
+    {:connect, state}
+  end
+  def handle_info({:join, topic}, transport, state) do
+    Logger.info("joining the topic #{topic}")
+    GenSocketClient.join(transport, topic)
+
+    {:ok, state}
+  end
+  def handle_info(message, _transport, state) do
+    Logger.warn("Unhandled message #{inspect message}")
+    {:ok, state}
+  end
+end
--- test/system/scos_system_test_test.exs
+++ test/system/scos_system_test_test.exs
@@ -2,11 +2,13 @@ defmodule ScosSystemTest do
   use ExUnit.Case
   @moduletag timeout: 1_200_000
   alias ScosSystemTest.Helpers
+  alias ScosSystemTest.StreamClient
 
   require Logger
 
   @temp_file_path "./tmp_file"
   # @discovery_url Application.get_env(:scos_system_test, :discovery_url)
+  @discovery_streams_url Application.get_env(:scos_system_test, :discovery_streams_url)
   @default_andi_url Application.get_env(:scos_system_test, :default_andi_url)
   @default_tdg_url Application.get_env(:scos_system_test, :default_tdg_url)
 
@@ -14,31 +16,50 @@ defmodule ScosSystemTest do
     File.rm(@temp_file_path)
     uuid = Helpers.generate_uuid()
 
+    Logger.info("Starting System Test with Dataset Id: #{uuid}")
+
     on_exit(fn ->
       Helpers.delete_dataset(uuid, @default_andi_url)
     end)
 
-    [uuid: uuid]
-  end
+    organization =
+      Helpers.generate_organization(uuid)
+      |> Helpers.upload_organization(@default_andi_url)
 
-  test "adds an organization and creates a dataset for it", %{uuid: uuid} do
-    record_count = 10
-
-    Logger.info("Starting System Test with Dataset Id: #{uuid}")
-
-    organization = Helpers.generate_organization(uuid)
-    organization_id = Helpers.upload_organization(organization, @default_andi_url)
-
-    Logger.info("Organization Id: #{organization_id}")
     Logger.info("Organization: #{inspect(organization)}")
 
-    dataset = Helpers.generate_dataset(uuid, organization_id, record_count, @default_tdg_url)
-    Logger.info("Dataset: #{inspect(dataset)}")
-    %{body: body} = Helpers.upload_dataset(dataset, @default_andi_url)
-    %{"technical" => %{"systemName" => system_name}} = Jason.decode!(body)
-    Logger.info("Result of dataset creation: #{inspect(body)}")
+    [uuid: uuid, organization: organization]
+  end
 
-    wait_for_data_to_appear_in_presto(system_name, record_count)
+  test "creates an ingest dataset", %{
+    uuid: uuid,
+    organization: organization
+  } do
+    record_count = 10
+
+    dataset = Helpers.generate_dataset(uuid, organization, record_count, @default_tdg_url) |> create_dataset()
+
+    wait_for_data_to_appear_in_presto(dataset.technical.systemName, record_count)
+  end
+
+  test "creates a streaming dataset and sees data in the stream", %{
+    uuid: uuid,
+    organization: organization
+  } do
+    record_count = 2
+
+    streaming_dataset =
+      Helpers.generate_dataset(uuid, organization, record_count, @default_tdg_url, %{
+        cadence: "*/2 * * * * *",
+        sourceType: "stream"
+      })
+      |> create_dataset()
+
+    stream_topic = "streaming:#{streaming_dataset.technical.systemName}"
+    {:ok, pid} = StreamClient.start_link(@discovery_streams_url)
+    StreamClient.join_topic(pid, stream_topic)
+
+    wait_for_data_to_appear_in_the_stream(stream_topic, record_count)
   end
 
   defp wait_for_data_to_appear_in_presto(system_name, count) do
@@ -49,6 +70,34 @@ defmodule ScosSystemTest do
     )
   end
 
+  defp wait_for_data_to_appear_in_the_stream(stream_topic, count) do
+    Patiently.wait_for!(
+      stored_messages_exceed_count(stream_topic, count),
+      dwell: 6_000,
+      max_tries: 50
+    )
+  end
+
+  defp stored_messages_exceed_count(stream_topic, expected_count) do
+    fn ->
+      try do
+        message_count =
+          :ets.tab2list(String.to_existing_atom(stream_topic))
+          |> Enum.map(fn {_, message} -> message end)
+          |> Enum.filter(fn message -> Map.has_key?(message, "quantity") end)
+          |> Enum.count()
+
+        assert message_count >= expected_count
+        Logger.info("Expected number of records on the stream was met or exceeded")
+        true
+      rescue
+        error ->
+          Logger.debug(Exception.format(:error, error, __STACKTRACE__))
+          false
+      end
+    end
+  end
+
   defp presto_query(system_name, message_count) do
     fn ->
       try do
@@ -57,11 +106,25 @@ defmodule ScosSystemTest do
           |> Helpers.execute()
 
         assert count == message_count
+        Logger.info("Expected number of records found in Presto")
         true
       rescue
-        _ ->
+        error ->
+          Logger.debug(Exception.format(:error, error, __STACKTRACE__))
           false
       end
+    end
+  end
+
+  defp create_dataset(proposed_dataset) do
+    Logger.debug("Creating Dataset: #{inspect(proposed_dataset)}")
+
+    with dataset_map <- Helpers.upload_dataset(proposed_dataset, @default_andi_url),
+         {:ok, dataset} <- SmartCity.Dataset.new(dataset_map) do
+      Logger.info("Created dataset #{dataset.business.dataTitle} with system name: #{dataset.technical.systemName}")
+      dataset
+    else
+      {:error, error} -> raise error
     end
   end
 
